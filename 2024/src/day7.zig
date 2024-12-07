41f029d86563ef5da4b1e7e73304c015c24b08cd
Louis Clergue <louis.clergue@gmail.com>
2024-12-07 23:37:15 +0100

    Search in reverse to prune branches

--- 2024/src/day7.zig
+++ 2024/src/day7.zig
@@ -4,15 +4,15 @@ const mem = std.mem;
 input: []const u8,
 allocator: mem.Allocator,
 
-const Solution = struct { p1: i64, p2: i64 };
+const Solution = struct { p1: u64, p2: u64 };
 
 pub fn solve(this: *const @This()) !Solution {
     var iter = mem.splitScalar(u8, this.input, '\n');
-    var rhs = try this.allocator.alloc(i64, 30); // there is no way we backtrack 30 elements
+    var rhs = try this.allocator.alloc(u64, 30); // there is no way we backtrack 30 elements
     defer this.allocator.free(rhs);
 
-    var p1: i64 = 0;
-    var p2: i64 = 0;
+    var p1: u64 = 0;
+    var p2: u64 = 0;
     while (iter.next()) |line| {
         if (line.len == 0) break;
         const lhs = consumeInt(line).?;
@@ -25,36 +25,38 @@ pub fn solve(this: *const @This()) !Solution {
             pending = res.rest[1..];
         }
         // std.debug.print("{d} to '{any}'\n", .{ lhs.val, rhs[0..i] });
-        if (canSolve(0, lhs.val, rhs[0..i])) {
+        if (canSolvePart1(lhs.val, rhs[0..i])) {
             p1 += lhs.val;
             p2 += lhs.val;
-        } else if (canSolve2(0, lhs.val, rhs[0..i])) p2 += lhs.val;
+        } else if (canSolvePart2(lhs.val, rhs[0..i])) p2 += lhs.val;
     }
     return Solution{ .p1 = p1, .p2 = p2 };
 }
 
-fn canSolve(partial: i64, lhs: i64, rhs: []const i64) bool {
-    if (rhs.len == 0) return partial == lhs;
-    if (partial > lhs) return false;
-    return canSolve(partial + rhs[0], lhs, rhs[1..]) or canSolve(partial * rhs[0], lhs, rhs[1..]);
+// try @mod/@rem and other div operations that may
+fn canSolvePart1(lhs: u64, rhs: []const u64) bool {
+    if (rhs.len == 1) return lhs == rhs[0];
+    const x = rhs[rhs.len - 1];
+    const rest = rhs[0 .. rhs.len - 1];
+    if (lhs > x and canSolvePart1(lhs - x, rest)) return true;
+    if (lhs % x == 0 and canSolvePart1(lhs / x, rest)) return true;
+    return false;
 }
 
-fn canSolve2(partial: i64, lhs: i64, rhs: []const i64) bool {
-    if (rhs.len == 0) return partial == lhs;
-    if (partial > lhs) return false;
-    return canSolve2(partial + rhs[0], lhs, rhs[1..]) // +
-    or canSolve2(partial * rhs[0], lhs, rhs[1..]) // *
-    or canSolve2(concat(partial, rhs[0]), lhs, rhs[1..]); // |
+fn canSolvePart2(lhs: u64, rhs: []const u64) bool {
+    if (rhs.len == 1) return lhs == rhs[0];
+    const x = rhs[rhs.len - 1];
+    const rest = rhs[0 .. rhs.len - 1];
+    if (lhs > x and canSolvePart2(lhs - x, rest)) return true;
+    if (lhs % x == 0 and canSolvePart2(lhs / x, rest)) return true;
+    var pow: u64 = 10;
+    while (pow <= x) pow *= 10;
+    if (lhs % pow == x and canSolvePart2(lhs / pow, rest)) return true;
+    return false;
 }
 
-fn concat(x: i64, y: i64) i64 {
-    var pow: i64 = 10;
-    while (pow <= y) pow *= 10;
-    return x * pow + y;
-}
-
-fn consumeInt(buf: []const u8) ?struct { rest: []const u8, val: i64 } {
-    var r: i64 = 0;
+fn consumeInt(buf: []const u8) ?struct { rest: []const u8, val: u64 } {
+    var r: u64 = 0;
     var i: usize = 0;
     while (i < buf.len and std.ascii.isDigit(buf[i])) : (i += 1)
         r = 10 * r + buf[i] - '0';
