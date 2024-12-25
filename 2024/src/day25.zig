27553e35b1f2589dd2b40ff01f8c63df854d3834
Louis Clergue <louis.clergue@gmail.com>
2024-12-25 09:37:55 +0100

    Solve day25
    Squashed commit of the following:
    
    commit 1282004e06ed1db5e9b312cf00907031074a9d86
    Author: Louis Clergue <louis.clergue@gmail.com>
    Date:   Wed Dec 25 09:34:15 2024 +0100
    
        Pass sample
    
    commit 1225120c84f2b2a38eda30cfd931c5a32638b661
    Author: Louis Clergue <louis.clergue@gmail.com>
    Date:   Wed Dec 25 09:29:34 2024 +0100
    
        Reading

---
+++ 2024/src/day25.zig
@@ -0,0 +1,97 @@
+const std = @import("std");
+const mem = std.mem;
+
+input: []const u8,
+allocator: mem.Allocator,
+
+const Solution = struct { p1: usize, p2: usize };
+
+const vec5 = @Vector(5, u4);
+
+pub fn solve(this: *const @This()) !Solution {
+    var iter = mem.splitScalar(u8, this.input, '\n');
+    var keys = std.ArrayList(vec5).init(this.allocator);
+    var locks = std.ArrayList(vec5).init(this.allocator);
+    defer keys.deinit();
+    defer locks.deinit();
+
+    while (iter.next()) |line| {
+        if (line.len == 0) continue; // skip empty line
+        var pins: vec5 = @splat(0);
+        var pin_line = iter.next().?;
+        for (0..5) |_| {
+            for (pin_line, 0..) |c, i| {
+                if (c == '#') pins[i] = pins[i] + 1;
+            }
+            pin_line = iter.next().?;
+        }
+        // std.debug.print("read {any}\n", .{pins});
+        if (pin_line[0] == '#') {
+            try keys.append(pins);
+        } else {
+            try locks.append(pins);
+        }
+    }
+
+    var p1: usize = 0;
+    const limit: vec5 = @splat(5);
+    for (keys.items) |key| {
+        for (locks.items) |lock| {
+            const check = (key + lock) <= limit;
+            if (@reduce(.And, check)) p1 += 1;
+        }
+    }
+    return Solution{ .p1 = p1, .p2 = 0 };
+}
+
+test "sample" {
+    const input =
+        \\#####
+        \\.####
+        \\.####
+        \\.####
+        \\.#.#.
+        \\.#...
+        \\.....
+        \\
+        \\#####
+        \\##.##
+        \\.#.##
+        \\...##
+        \\...#.
+        \\...#.
+        \\.....
+        \\
+        \\.....
+        \\#....
+        \\#....
+        \\#...#
+        \\#.#.#
+        \\#.###
+        \\#####
+        \\
+        \\.....
+        \\.....
+        \\#.#..
+        \\###..
+        \\###.#
+        \\###.#
+        \\#####
+        \\
+        \\.....
+        \\.....
+        \\.....
+        \\#....
+        \\#.#..
+        \\#.#.#
+        \\#####
+        \\
+    ;
+    const problem: @This() = .{
+        .input = input,
+        .allocator = std.testing.allocator,
+    };
+    const sol = try problem.solve();
+    try std.testing.expectEqual(3, sol.p1);
+    // try std.testing.expectEqual(123, sol.p2);
+}
