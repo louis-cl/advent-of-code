e72c8c84d857d664077b3f8ab33ad24bdfd9af98
Louis Clergue <louis.clergue@gmail.com>
2024-12-29 14:50:25 +0100

    Improve day14
    Squashed commit of the following:
    
    commit 50979b8f96003904d054e0b226b4827b95c0c70b
    Author: Louis Clergue <louis.clergue@gmail.com>
    Date:   Sun Dec 29 14:47:28 2024 +0100
    
        Switch coordinates to u32
    
    commit 8a3423334cb3f5a0e21907e57df3f699b0a9812d
    Author: Louis Clergue <louis.clergue@gmail.com>
    Date:   Sun Dec 29 14:30:51 2024 +0100
    
        make tests pass (not hang in part2)
    
    commit fad1e1065469512dac6a6d28b96ac34b5de95c0d
    Author: Louis Clergue <louis.clergue@gmail.com>
    Date:   Sun Dec 29 14:26:40 2024 +0100
    
        Refactor into struct
    
    commit 2f32db2106391a44e798e3884910dc7b4897a9fd
    Author: Louis Clergue <louis.clergue@gmail.com>
    Date:   Sun Dec 29 14:15:36 2024 +0100
    
        Extract part1, print tree vertically

--- 2024/src/day14.zig
+++ 2024/src/day14.zig
@@ -5,92 +5,110 @@ input: []const u8,
 allocator: mem.Allocator,
 
 const Solution = struct { p1: u32, p2: u32 };
-const vec2 = @Vector(2, i32);
+const vec2 = @Vector(2, u32);
+
+const RobotMap = struct {
+    robots: std.ArrayList([2]vec2),
+    width: u32,
+    height: u32,
+    fn init(allocator: mem.Allocator, w: u32, h: u32) @This() {
+        return @This(){
+            .robots = std.ArrayList([2]vec2).init(allocator),
+            .width = w,
+            .height = h,
+        };
+    }
+
+    fn read(this: *@This(), input: []const u8) void {
+        var iter = mem.splitScalar(u8, input, '\n');
+        while (iter.next()) |line| {
+            if (line.len == 0) break;
+            const robot = this.parseLine(line);
+            // std.debug.print("{any}\n", .{robot});
+            this.robots.append(robot) catch unreachable; // let's assume the tree is after 100 steps
+        }
+    }
+
+    fn parseLine(this: *const @This(), line: []const u8) [2]vec2 {
+        var iter = mem.splitAny(u8, line, "=, ");
+        _ = iter.next().?; // "p="
+        const x = std.fmt.parseUnsigned(u16, iter.next().?, 10) catch unreachable;
+        const y = std.fmt.parseUnsigned(u16, iter.next().?, 10) catch unreachable;
+        _ = iter.next().?; // " v="
+        const vx = std.fmt.parseInt(i32, iter.next().?, 10) catch unreachable;
+        const vy = std.fmt.parseInt(i32, iter.next().?, 10) catch unreachable;
+        const w: i32 = @intCast(this.width);
+        const h: i32 = @intCast(this.height);
+        return .{ vec2{ x, y }, vec2{ @intCast(@mod(vx, w)), @intCast(@mod(vy, h)) } };
+    }
+
+    fn part1(this: *const @This()) u32 {
+        var quadrants: [4]u32 = .{ 0, 0, 0, 0 };
+        for (this.robots.items) |robot| {
+            const end = this.move(robot, 100);
+            if (this.quadrant(end)) |q| quadrants[q] += 1;
+        }
+        return quadrants[0] * quadrants[1] * quadrants[2] * quadrants[3];
+    }
+
+    fn quadrant(this: *const @This(), robot: vec2) ?usize {
+        const midw = (this.width - 1) / 2;
+        const midh = (this.height - 1) / 2;
+        if (robot[0] == midw or robot[1] == midh) return null;
+        var ret: usize = if (robot[0] <= midw) 0 else 1;
+        ret += if (robot[1] <= midh) 0 else 2;
+        return ret;
+    }
+
+    fn part2(this: *const @This()) u32 {
+        // idea: if an image is formed robots must be close to each other
+        // make 10x10 buckets and metrics is sum of count per bucket ^ 2, find a threshold manually and print
+        var buckets = [_]u32{0} ** 121; // 11 x 11;
+        var metric: u64 = 0;
+        var count: u32 = 0;
+        while (metric < 7200) : (count += 1) { // do one iteration
+            for (this.robots.items, 0..) |rob, i| {
+                const next_pos = this.move(rob, 1);
+                this.robots.items[i][0] = next_pos;
+                buckets[@intCast(@divTrunc(next_pos[0], 10) * 10 + @divTrunc(next_pos[1], 10))] += 1;
+            }
+            metric = 0;
+            for (buckets, 0..) |b, i| {
+                metric += b * b;
+                buckets[i] = 0; // reset
+            }
+        }
+        this.print();
+        return count;
+    }
+
+    fn move(this: *const @This(), robot: [2]vec2, steps: u32) vec2 {
+        const end = robot[0] + robot[1] * vec2{ steps, steps };
+        return vec2{ end[0] % this.width, end[1] % this.height };
+    }
+
+    fn print(this: *const @This()) void {
+        for (0..this.height) |y| {
+            for (0..this.width) |x| {
+                var char: u8 = '.';
+                for (this.robots.items) |rob| {
+                    if (std.meta.eql(rob[0], vec2{ @intCast(x), @intCast(y) })) {
+                        char = '#';
+                        break;
+                    }
+                }
+                std.debug.print("{c}", .{char});
+            }
+            std.debug.print("\n", .{});
+        }
+    }
+};
 
 pub fn solve(this: *const @This()) !Solution {
-    return solve2(this, 101, 103);
-}
-
-fn solve2(this: *const @This(), width: i32, height: i32) Solution {
-    var robots = std.ArrayList([2]vec2).init(this.allocator);
-    defer robots.deinit();
-    var iter = mem.splitScalar(u8, this.input, '\n');
-    var quadrants: [4]u32 = .{ 0, 0, 0, 0 };
-    while (iter.next()) |line| {
-        if (line.len == 0) break;
-        const robot = parseLine(line);
-        // std.debug.print("{any}\n", .{robot});
-        const end = move(robot, 100, width, height);
-        if (quadrant(end, width, height)) |q| quadrants[q] += 1;
-        robots.append(.{ end, robot[1] }) catch unreachable; // let's assume the tree is after 100 steps
-    }
-    // std.debug.print("{any}\n", .{quadrants});
-    return Solution{ .p1 = quadrants[0] * quadrants[1] * quadrants[2] * quadrants[3], .p2 = 100 + part2(robots.items, width, height) };
-}
-
-fn move(robot: [2]vec2, steps: i32, width: i32, height: i32) vec2 {
-    const end = robot[0] + robot[1] * vec2{ steps, steps };
-    return vec2{ @mod(end[0], width), @mod(end[1], height) };
-}
-
-fn quadrant(robot: vec2, width: i32, height: i32) ?usize {
-    const midw = @divExact(width - 1, 2);
-    const midh = @divExact(height - 1, 2);
-    if (robot[0] == midw or robot[1] == midh) return null;
-    var ret: usize = if (robot[0] <= midw) 0 else 1;
-    ret += if (robot[1] <= midh) 0 else 2;
-    return ret;
-}
-
-fn part2(robots: [][2]vec2, width: i32, height: i32) u32 {
-    _ = width + height;
-    // idea: if an image is formed robots must be close to each other
-    // make 10x10 buckets and metrics is sum of count per bucket ^ 2, find a threshold manually and print
-    var buckets = [_]u32{0} ** 121; // 11 x 11;
-    var metric: u64 = 0;
-    var count: u32 = 0;
-    while (metric < 7200) : (count += 1) { // do one iteration
-        for (robots, 0..) |rob, i| {
-            const next_pos = move(rob, 1, width, height);
-            robots[i][0] = next_pos;
-            buckets[@intCast(@divTrunc(next_pos[0], 10) * 10 + @divTrunc(next_pos[1], 10))] += 1;
-        }
-        metric = 0;
-        for (buckets, 0..) |b, i| {
-            metric += b * b;
-            buckets[i] = 0; // reset
-        }
-    }
-    std.debug.print("THE MAP {d}\n", .{count});
-    print(robots, @intCast(width), @intCast(height));
-    return count;
-}
-
-fn print(robots: [][2]vec2, width: usize, height: usize) void {
-    for (0..width) |x| {
-        for (0..height) |y| {
-            var char: u8 = '.';
-            for (robots) |rob| {
-                if (std.meta.eql(rob[0], vec2{ @intCast(x), @intCast(y) })) {
-                    char = '#';
-                    break;
-                }
-            }
-            std.debug.print("{c}", .{char});
-        }
-        std.debug.print("\n", .{});
-    }
-}
-
-fn parseLine(line: []const u8) [2]vec2 {
-    var iter = mem.splitAny(u8, line, "=, ");
-    _ = iter.next().?; // "p="
-    const x = std.fmt.parseUnsigned(i32, iter.next().?, 10) catch unreachable;
-    const y = std.fmt.parseUnsigned(i32, iter.next().?, 10) catch unreachable;
-    _ = iter.next().?; // " v="
-    const vx = std.fmt.parseInt(i32, iter.next().?, 10) catch unreachable;
-    const vy = std.fmt.parseInt(i32, iter.next().?, 10) catch unreachable;
-    return .{ vec2{ x, y }, vec2{ vx, vy } };
+    var robotMap = RobotMap.init(this.allocator, 101, 103);
+    defer robotMap.robots.deinit();
+    robotMap.read(this.input);
+    return Solution{ .p1 = robotMap.part1(), .p2 = robotMap.part2() };
 }
 
 test "sample" {
@@ -109,10 +127,9 @@ test "sample" {
         \\p=9,5 v=-3,-3
         \\
     ;
-    const problem: @This() = .{
-        .input = input,
-        .allocator = std.testing.allocator,
-    };
-    const sol = problem.solve2(11, 7);
-    try std.testing.expectEqual(12, sol.p1);
+
+    var robotMap = RobotMap.init(std.testing.allocator, 11, 7);
+    defer robotMap.robots.deinit();
+    robotMap.read(input);
+    try std.testing.expectEqual(12, robotMap.part1());
 }
